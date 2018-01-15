3b9653874b2f6cc7360121fac2f8fba0afcef1f0
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-01-15 13:29:16 -0800

    added variables for 1st and Pike

--- js/app.js
+++ js/app.js
@@ -7,14 +7,27 @@ hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '
 
 
 // Create first object literal: 1st and Pike
+var firstAndPike  = {
+  // 1. store min hourly customer
+  minCust_Hour: 23,
+  // 2. store max hourly customer
+  maxCust_Hour: 65,
+  // 3. store average Cooks per customer
+  avgCookies_Customer: 6.3,
+  // 4. write method to determine random number of cookies per hour based on 1, 2, 3 above
+ 
+  // 5. use above method and write values to an array for each hour
+  // 5.1. store that array in a variable
+  // 6. loop through array of strings and avgCustomers and write elements to HTML
+  
+}
 
-// 1. store min hourly customer
-// 2. store max hourly customer
-// 3. store average Cooks per customer
-// 4. write method to determine random number of cookies per hour based on 1, 2, 3 above
-// 5. use above method and write values to an array for each hour
-// 5.1. store that array in a variable
-// 6. loop through array of strings and avgCustomers and write elements to HTML
+
+
+
+
+
+
 
 
 // copied intial comments for reference
