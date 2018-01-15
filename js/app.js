8d72316a426fb502d3c649ab8ce095f5633f51f8
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-01-15 13:23:35 -0800

    added app.js and psuedo-code to it planning out code

---
+++ js/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// global variables here
+// instantial array of strings, 6am -> 8pm
+hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+
+
+// Create first object literal: 1st and Pike
+
+// 1. store min hourly customer
+// 2. store max hourly customer
+// 3. store average Cooks per customer
+// 4. write method to determine random number of cookies per hour based on 1, 2, 3 above
+// 5. use above method and write values to an array for each hour
+// 5.1. store that array in a variable
+// 6. loop through array of strings and avgCustomers and write elements to HTML
+
+
+// copied intial comments for reference
+// Create first object literal: 1st and Pike
+
+// 1. store min hourly customer
+// 2. store max hourly customer
+// 3. store average Cooks per customer
+// 4. write method to determine random number of cookies per hour based on 1, 2, 3 above
+// 5. use above method and write values to an array for each hour
+// 5.1. store that array in a variable
+// 6. loop through array of strings and avgCustomers and write elements to HTML
