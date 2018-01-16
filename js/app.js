e291807b7e8254db74963a5c1ae06778f86a44f6
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-01-15 21:10:52 -0800

    styling changes and updated app.js

--- js/app.js
+++ js/app.js
@@ -66,7 +66,6 @@ var seaTacAirport = {
     ulEl.appendChild(liEl);
   }
 };
-// console.log(seaTacAirport.randomNumCustomers_Hour());
 seaTacAirport.render();
 
 /* Seattle Center */
@@ -144,42 +143,3 @@ var alki = {
   }
 };
 alki.render();
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* <h2>1st and Pike</h3>
-<ul id="seaTacAirport"></ul>
-<ul id="seattleCenter"></ul>
-<ul id="capitolHill"></ul>
-<ul id="alki"></ul> */
-
-
-// render lists on page
-
-
-
-
-
-
-
-
-// copied intial comments for reference
-// Create first object literal: 1st and Pike
-
-// 1. store min hourly customer
-// 2. store max hourly customer
-// 3. store average Cooks per customer
-// 4. write method to determine random number of cookies per hour based on 1, 2, 3 above
-// 5. use above method and write values to an array for each hour
-// 5.1. store that array in a variable
-// 6. loop through array of strings and avgCustomers and write elements to HTML
