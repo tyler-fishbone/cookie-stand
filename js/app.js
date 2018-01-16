9631dbfbfe50f9d6966117c64355f588a06040c0
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-01-16 15:12:20 -0800

    all 5 locations and sales information writing to sales page table! Inlcudes header and table data but no totals table footer

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 // global variables here
 // instantial array of strings, 6am -> 8pm
-var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hoursOfOperation = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
 var arrayOfLocations = [];
 var salesTable = document.getElementById('locations');
 
@@ -43,19 +43,47 @@ Location.prototype.render = function () {
   salesTable.appendChild(trEl);
 };
 
-//code for testing
-var testLocation = new Location('testLocation', 6, 10, 3);
+function makeHeaderRow (arrayForHeader) {
+  //new row, empty first cell
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  thEl.textContent = '';
+  trEl.appendChild(thEl);
+  // loop through hours of operation
+  for(var i = 0; i < hoursOfOperation.length; i++){
+    thEl = document.createElement('th');
+    thEl.textContent = arrayForHeader[i];
+    trEl.appendChild(thEl);
+  }
+  thEl = document.createElement('th');
+  thEl.textContent = 'Daily Location Total';
+  trEl.appendChild(thEl);
+  //append everything into table row
+  salesTable.appendChild(trEl);
+}
+
+//construct objects
 var firstAndPike = new Location('1st and Pike', 23, 65, 6.3);
 var seaTacAirport = new Location('Seatac Airport', 3, 24, 1.2);
 var seattleCenter = new Location('Seattle Center', 11, 38, 3.7);
 var capitolHill = new Location('Capitol Hill', 20, 38, 2.3);
 var alki = new Location('Alki', 2, 16, 4.6);
-testLocation.render();
+
+//make table
+makeHeaderRow(hoursOfOperation);
 firstAndPike.render();
 seaTacAirport.render();
 seattleCenter.render();
 capitolHill.render();
 alki.render();
+
+
+
+
+
+
+
+
 
 
 
